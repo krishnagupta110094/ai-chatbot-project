d1014d62aa77659c36e127efe00fe589471cc709
krishnagupta110094 <krishnagupta110094@gmail.com>
2026-02-15 11:58:45 +0530

    update url

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -12,7 +12,7 @@ const { createMemory, queryMemory } = require("../services/vector.service");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     cors: {
-      origin: "https://ai-chatbot-frontend-lqkd.onrender.com",
+      origin: "https://ai-chatbot-frontend-zeta-ashy.vercel.app",
       credentials: true, // REQUIRED for cookies
     },
   });
